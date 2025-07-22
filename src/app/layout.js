f35c06426ff83b3c8b33ef5fd39c30ad03e0013a
Parul Thakur <paru.thakur11oct@gmail.com>
2025-07-22 22:20:39 +0100

    fixed fonts error

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,22 @@
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-import { sourceSansPro } from "./fonts";
- import "./globals.css";
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
 
 export const metadata = {
-  title: "Parul Thakur",
-  description: "Portfolio Website",
-   icons: {
-    icon: "/favicon.ico", 
-   },
+  title: 'Parul Thakur',
+  description: 'Portfolio Website',
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={sourceSansPro.className}>
+    <html lang="en" className={inter.className}>
       <body>{children}</body>
     </html>
   );
